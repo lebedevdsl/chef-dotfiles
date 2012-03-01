2f584a0319613b0b1f1151f417edc64bb7d0b0dd
Alisdair Lebedev <alisdairgm@gmail.com>
2012-03-01 06:00:13 +0400

    same

---
+++ libraries/backup_file.rb
@@ -0,0 +1,9 @@
+class Chef
+	class Recipe
+		def backup(entry)
+			if File.exists?("#{home}/#{entry}") and not File.symlink?("#{home}/#{entry}")
+				Fileutils.mv("#{home}/#{entry}","#{home}/.dotfiles/#{entry}.#{Date.now}.bckp")
+			end
+		end
+	end
+end
