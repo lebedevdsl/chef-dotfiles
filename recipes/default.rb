a000db66364e80651303c6539cfbea7611a6589e
Alisdair Lebedev <alisdairgm@gmail.com>
2012-03-01 04:12:32 +0400

    fixes, added conditionals

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,17 +10,34 @@ admins.each do |login|
 	admin = data_bag_item("admins", login)
 	home = "/home/#{login}"
 	
-	unless File.directory?(home)
+	# Exporting files only if user realy wants to
+	unless admin["dotfiles"]["enabled"] and admin["dotfiles"]["enabled"] == true
+		# Exporting standard dotfiles only if home_directory exists	
 		git "#{home}/.dotfiles" do
-			repository :standart_dotfiles
-			action :sync
+			repository :standard_dotfiles
+			action :export
+			only_if {File.directory?(home)}
+		end
+		
+		Dir.foreach("#{home}/.dotfiles") do |entry|
+			link "#{home}/#{entry}" do
+				to entry
+			end
 		end
 
-	unless admin["custom_dotfiles_repo"].nil?
+		# Uploading all user's custom dotfiles if present
 		git "#{home}/.custom_dotfiles" do
-			repository admin["custom_dotfiles_repo"]
+			repository admin["dotfiles"]["custom_dotfiles_repo"]
 			user login
 			action :sync
+			not_if { admin["dotfiles"]["custom_dotfiles_repo"].nil? }
+			only_if {File.directory?(home)}
 		end
-
+		
+		admin["dotfiles"].each do |custom_entry|
+			link "#{home}/#{custom_entry}" do
+				to "#{home}/.custom_dotfiles/#{custom_entry}"
+			end
+		end
+	end
 end
