aa9a571362368acd6a117bf4ada1378f1fc7468e
Alisdair Lebedev <alisdairgm@gmail.com>
2012-03-04 02:43:01 +0400

    done

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default[:dotfiles] = {
-	:files => [".bashrc",".zshrc",".vimrc"],
-	:standard_repository => "git://github.com/lebedevdsl/my-dotfiles.git"
+  :files => [".bashrc",".zshrc",".vimrc"],
+  :standard_repository => "git://github.com/lebedevdsl/my-dotfiles.git"
 }
