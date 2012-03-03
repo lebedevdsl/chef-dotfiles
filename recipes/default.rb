5778c8d157b8060dffc5eeffb5ad0bcf09da28d4
Alisdair Lebedev <alisdairgm@gmail.com>
2012-03-04 02:39:58 +0400

    retabbed recipes/default

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,82 +7,82 @@
 
 # Installing required git core-packages
 package 'git-core' do
-	action :install
-	options '--force-yes'
+  action :install
+  options '--force-yes'
 end
 
 # Retrieving admins collection
 admins = data_bag("admins")
 # logging debug_info
 log "[dotfiles] 1. #{admins}" do
-	level :debug
+  level :debug
 end
 
 # admins list round-trip
 admins.each do |login|
-	admin = data_bag_item("admins", login)
-	home = "/home/#{login}"
-	
-	#logging debug info
-	log "[dotfiles] 2. #{admin} #{home}" do
-		level :debug
-	end
-	
-	#logging debug info
-	log "[dotfiles] 3. #{admin['dotfiles']['enabled']}" do
-		level :debug
-	end
-	
-	# Exporting files only if user realy wants to
-	if admin['dotfiles']['enabled_standard']
-		# Exporting standard dotfiles only if home_directory exists	
-		log "[dotfiles] Uploading STANDARD dotfiles for #{admin} from #{node[:dotfiles][:standard_repository]} to #{home}/.dotfiles " do
-			level :info
-		end
-		
-		git "#{home}/.dotfiles" do
-			repository node[:dotfiles][:standard_repository]
-			action :sync
-			user login
-			group login
-			only_if {File.directory?(home)}
-		end
-		
-		node[:dotfiles][:files].each do |entry|
-			link "#{home}/#{entry}" do
-				owner login
-				group login
-				to "#{home}/.dotfiles/#{entry}"
-			end
-		end
-		
-		log "[dotfiles] Default dotfiles successfuly exported from #{node[:dotfiles][:standard_repository]}" do
-			level :info
-		end
-	end
-	
-	# Or/and uploading custom dotfiles from private repository
-	if admin['dotfiles']['enabled_custom']	
-		
-		log "[dotfiles] Uploading CUSTOM dotfiles for #{admin} from #{admin['dotfiles']['custom_dotfiles_repo']} to #{home}/.custom_dotfiles " do
-			level :info
-		end
-		
-		git "#{home}/.custom_dotfiles" do
-			repository admin['dotfiles']['custom_dotfiles_repo']
-			user login
-			group login
-			action :sync
-			not_if { admin['dotfiles']['custom_dotfiles_repo'].nil? }
-			only_if {File.directory?(home)}
-		end
-		
-		admin['dotfiles']['custom_dotfiles'].each do |entry|
-			link "#{home}/#{entry}" do
-				owner login
-				group login
-				to "#{home}/.custom_dotfiles/#{entry}"
-			end
-		end
-	end
+  admin = data_bag_item("admins", login)
+  home = "/home/#{login}"
+  
+  #logging debug info
+  log "[dotfiles] 2. #{admin} #{home}" do
+    level :debug
+  end
+  
+  #logging debug info
+  log "[dotfiles] 3. #{admin['dotfiles']['enabled']}" do
+    level :debug
+  end
+  
+  # Exporting files only if user realy wants to
+  if admin['dotfiles']['enabled_standard']
+    # Exporting standard dotfiles only if home_directory exists 
+    log "[dotfiles] Uploading STANDARD dotfiles for #{admin} from #{node[:dotfiles][:standard_repository]} to #{home}/.dotfiles " do
+      level :info
+    end
+    
+    git "#{home}/.dotfiles" do
+      repository node[:dotfiles][:standard_repository]
+      action :sync
+      user login
+      group login
+      only_if {File.directory?(home)}
+    end
+    
+    node[:dotfiles][:files].each do |entry|
+      link "#{home}/#{entry}" do
+        owner login
+        group login
+        to "#{home}/.dotfiles/#{entry}"
+      end
+    end
+    
+    log "[dotfiles] Default dotfiles successfuly exported from #{node[:dotfiles][:standard_repository]}" do
+      level :info
+    end
+  end
+  
+  # Or/and uploading custom dotfiles from private repository
+  if admin['dotfiles']['enabled_custom']  
+    
+    log "[dotfiles] Uploading CUSTOM dotfiles for #{admin} from #{admin['dotfiles']['custom_dotfiles_repo']} to #{home}/.custom_dotfiles " do
+      level :info
+    end
+    
+    git "#{home}/.custom_dotfiles" do
+      repository admin['dotfiles']['custom_dotfiles_repo']
+      user login
+      group login
+      action :sync
+      not_if { admin['dotfiles']['custom_dotfiles_repo'].nil? }
+      only_if {File.directory?(home)}
+    end
+    
+    admin['dotfiles']['custom_dotfiles'].each do |entry|
+      link "#{home}/#{entry}" do
+        owner login
+        group login
+        to "#{home}/.custom_dotfiles/#{entry}"
+      end
+    end
+  end
 end
