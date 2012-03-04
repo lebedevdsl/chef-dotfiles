38123c7477202475b27fb8657f02d79a87eebd35
Alisdair Lebedev <alisdairgm@gmail.com>
2012-03-04 04:09:31 +0400

    fixed metadata

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ maintainer       "Oversun-Scalaxy LTD"
 maintainer_email "d.lebedev@scalaxy.ru"
 license          "apachev2"
 description      "Installs/Configures dotfiles"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.textile'))
 version          "0.0.1"
