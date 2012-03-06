ce483acd9a61a8090c7c8d68484ea2b47b2d908d
Alisdair Lebedev <alisdairgm@gmail.com>
2012-03-06 19:14:51 +0400

    Fixed cookbook version

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "d.lebedev@scalaxy.ru"
 license          "apachev2"
 description      "Installs/Configures dotfiles"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.textile'))
-version          "0.0.1"
+version          "0.1.0"
