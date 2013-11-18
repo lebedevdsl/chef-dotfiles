e393f222284d4f80adb8c8a015fe97d915368e5a
William Bert <william@spanishdict.com>
2013-11-18 10:51:09 -0500

    SD-specific default attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,8 @@
 default['dotfiles'] = {
-  :files => [".bashrc",".zshrc",".vimrc"],
-  :standard_repository => "git://github.com/lebedevdsl/my-dotfiles.git",
-  :data_bag => 'admins',
-  :custom_dotfiles_dir => '.custom_dotfiles',
+  :files => [], # Customize per user.
+  :standard_repository => Nil, # Don't use.
+  :enabled_standard -> false,
+  :data_bag => 'users',
+  :custom_dotfiles_dir => 'dotfiles',
   :custom_dotfiles_repo_prefix => Nil
 }
